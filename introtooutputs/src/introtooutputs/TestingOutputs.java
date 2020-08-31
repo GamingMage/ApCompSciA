a82086016ce3224464b6632e8a9548019e1487ff
GamingMage <josephliang@roadrunner.com>
2020-08-31 15:43:03 -0400

    Intro To Outputs Homework
    Roughly center your name
    Blank line
    Your name 3 times(one line of code)
    Blank line
    Java in a box

---
+++ introtooutputs/src/introtooutputs/TestingOutputs.java
@@ -0,0 +1,22 @@
+package introtooutputs;
+
+public class TestingOutputs 
+{
+	static int StateMachineFlow = 0;
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("								    Joseph Liang");
+		System.out.println("");
+		while(StateMachineFlow < 3) {
+			System.out.println("Joseph Liang");
+			StateMachineFlow++;
+		}
+		System.out.println("");
+		System.out.println("********");
+		System.out.println("**java**");
+		System.out.println("********");
+
+	}
+
+}
