c6bf38015acc59facadb566b4dfe4aba7646c1c7
GamingMage <josephliang@roadrunner.com>
2020-09-02 15:22:47 -0400

    Escape Characters Practice

---
+++ escapecharacters/src/escapecharacters/ASCIIRabbit.java
@@ -0,0 +1,14 @@
+package escapecharacters;
+/*
+ * Joseph Liang
+ * 9/2/20
+ * This program prints a text rabbit sitting on a box
+ */
+public class ASCIIRabbit {
+
+	public static void main(String[] args) {
+		System.out.println("(\\(\\\n(o.o)\no (\")(\")\n--------|\n        |\n        |\n________|");
+
+	}
+
+}
---
+++ escapecharacters/src/escapecharacters/AsimovQuote.java
@@ -0,0 +1,14 @@
+package escapecharacters;
+/*
+ * Joseph Liang
+ * 9/2/20
+ * This program prints an Issac Asimov quote
+ */
+public class AsimovQuote {
+
+	public static void main(String[] args) {
+		System.out.println("\"I don't fear computers.\n I fear the lack of them.\"\n     - Issac Asimov");
+
+	}
+
+}
