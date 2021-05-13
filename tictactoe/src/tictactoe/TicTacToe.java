8b181a8ae8abd0bc0c1100f7fff25e36c529cb09
GamingMage <josephliang@roadrunner.com>
2021-05-13 08:06:36 -0500

    Almost working Tic Tac Toe
    sometimes

--- tictactoe/src/tictactoe/TicTacToe.java
+++ tictactoe/src/tictactoe/TicTacToe.java
@@ -17,36 +17,51 @@ public class TicTacToe {
 		}
 	}
 	public String coinToss(int x) {
-		int n = (int)(Math.random() * 1 - 0 + 1) + 0;
+		int n = (int)(Math.random() * (1 - 0 + 1)) + 0;
 		if(x >= 1 && n == 1) {
 			first = true;
+			return "You guessed correct! You take the first turn!";
 		}else if (x <= 0 && n == 0) {
 			first = true;
+			return "You guessed correct! You take the first turn!";
 		}else {
 			first = false;
+			return "You guessed incorrect. The computer will go first.";
 		}
-		return "";
 	}
 	
 	public String getBoard() {
 		String out = "  0 1 2";
 		for(int i = 0; i < board.length; i++) {
-			out += "\n" + (i + 1) + " ";
+			out += "\n" + (i) + " ";
 			for(int j = 0; j < board[0].length; j++) {
 				out += board[i][j] + " ";
 			}
 		}
+		out += "\n";
 		return out;
 	}
+	public boolean getFirst() {
+		return first;
+	}
 	
-	public void humanTurn(int[] a) {
+	public void playerTurn(int[] a) {
 		updateBoard(a, first);
 	}
 	
 	public void botTurn() {
-		int[] a = new int[2];
-		
-		updateBoard(a, !first);
+		int[] a = checkBoard();
+		if (a[0] != -1 && a[1] != -1) {
+			updateBoard(a, !first);
+		}else if (a[0] == -1){
+			int[] b = checkCorners();
+			if (b[0] != -1 && b[1] != -1) {
+				updateBoard(b, !first);
+			}
+		}else {
+			int[] c = findEmpty();
+			updateBoard(c, !first);
+		}
 	}
 	
 	private void updateBoard(int[] a, boolean x) {
@@ -57,7 +72,97 @@ public class TicTacToe {
 		}
 	}
 	
-	private boolean checkWin() {
+	private int[] checkBoard() {
+		int[] a = {-1,-1};
+		//check columns for 2 of the same
+		for(int i = 0; i < board.length; i++) {
+			if (board[0][i].equals(board[1][i]) && board[2][i].equals("_")) {
+				a[0] = 2;
+				a[1] = i;
+			}else if (board[1][i].equals(board[2][i]) && board[0][i].equals("_")) {
+				a[0] = 0;
+				a[1] = i;
+			}else if (board[0][i].equals(board[2][i]) && board[1][i].equals("_")) {
+				a[0] = 1;
+				a[1] = i;
+			}
+		}
+		//check rows for two in a row
+		for(int i = 0; i < board.length; i++) {
+			if (board[i][0].equals(board[i][1]) && board[i][2].equals("_")) {
+				a[0] = i;
+				a[1] = 2;
+			}else if (board[i][1].equals(board[i][2]) && board[i][0].equals("_")) {
+				a[0] = i;
+				a[1] = 0;
+			}else if (board[i][0].equals(board[i][2]) && board[i][1].equals("_")) {
+				a[0] = i;
+				a[1] = 1;
+			}
+		}
+		//check diagonals for two in a row
+		if (board[0][0].equals(board[1][1]) && board[2][2].equals("_")) {
+			a[0] = 2;
+			a[1] = 2;
+		}else if (board[1][1].equals(board[2][2]) && board[0][0].equals("_")) {
+			a[0] = 0;
+			a[1] = 0;
+		}else if (board[0][2].equals(board[1][1]) && board[2][0].equals("_")) {
+			a[0] = 2;
+			a[1] = 0;
+		}else if (board[1][1].equals(board[2][0]) && board[0][2].equals("_")) {
+			a[0] = 0;
+			a[1] = 2;
+		}else if (board[0][0].equals(board[2][2]) && board[1][1].equals("_")) {
+			a[0] = 1;
+			a[1] = 1;
+		}else if (board[0][2].equals(board[2][0]) && board[1][1].equals("_")) {
+			a[0] = 1;
+			a[1] = 1;
+		}
+		return a;
+	}
+	private int[] checkCorners() {
+		int[] a = {-1,-1};
+		if (board[0][0].equals("_")) {
+			a[0] = 0;
+			a[1] = 0;
+		}else if (board[0][2].equals("_")) {
+			a[0] = 0;
+			a[1] = 2;
+		}else if (board[2][0].equals("_")) {
+			a[0] = 2;
+			a[1] = 0;
+		}else if (board[2][2].equals("_")) {
+			a[0] = 2;
+			a[1] = 2;
+		}
+		return a;
+	}
+	private int[] findEmpty() {
+		int[] a = {-1, -1};
+		for(int i = 0; i < board.length; i++) {
+			for(int j = 0; j < board[0].length; j++) {
+				if (board[i][j].equals("_")) {
+					a[0] = i;
+					a[1] = j;
+				}
+			}
+		}
+		return a;
+	}
+	
+	public boolean checkDraw() {
+		for(int i = 0; i < board.length; i++) {
+			for(int j = 0; j < board[0].length; j++) {
+				if (board[i][j].equals("_")) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	public boolean checkWin() {
 		if (checkColumns()) {
 			return true;
 		}else if (checkRows()) {
@@ -69,7 +174,7 @@ public class TicTacToe {
 	}
 	private boolean checkColumns() {
 		for(int i = 0; i < board.length; i++) {
-			if (board[0][i].equals(board[1][i]) && board[1][i].equals(board[2][i])) {
+			if (board[0][i].equals(board[1][i]) && board[1][i].equals(board[2][i]) && !board[0][i].equals("_")) {
 				return true;
 			}
 		}
@@ -77,17 +182,17 @@ public class TicTacToe {
 	}
 	private boolean checkRows() {
 		for(int i = 0; i < board.length; i++) {
-			if (board[i][0].equals(board[i][1]) && board[i][1].equals(board[i][2])) {
+			if (board[i][0].equals(board[i][1]) && board[i][1].equals(board[i][2]) && !board[i][0].equals("_")) {
 				return true;
 			}
 		}
 		return false;
 	}
 	private boolean checkDiagonal() {
-		if (board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2])) {
+		if (board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]) && !board[0][0].equals("_")) {
 			return true;
 		}
-		if (board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0])) {
+		if (board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0]) && !board[0][2].equals("_")) {
 			return true;
 		}
 		return false;
--- tictactoe/src/tictactoe/TicTacToeGame.java
+++ tictactoe/src/tictactoe/TicTacToeGame.java
@@ -1,16 +1,64 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
 	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
 		TicTacToe game = new TicTacToe();
 		int[] input = new int[2];
+		boolean playing = true;
+		boolean win = false;
+		boolean draw = false;
+		boolean playerTurn = false;
+		int x = 0;
 		
-		input[0] = 0;
-		input[1] = 0;
-		System.out.println(game.getBoard());
-		game.humanTurn(input);
-		System.out.println(game.getBoard());
+		System.out.println("Shall we play a game?\nPress enter to begin.");
+		userInput.nextLine();
+		while (playing) {
+			System.out.print("The first turn shall be decided with a coin toss.\nPlease enter 0 for tails and 1 for heads: ");
+			System.out.println(game.coinToss(userInput.nextInt()));
+			playerTurn = game.getFirst();
+			System.out.println(game.getBoard());
+			System.out.println("Please enter your space choice in the form 'row number' 'column number' with a space in between.\n");
+			userInput.nextLine();
+			while (!win && !draw) {
+				if (playerTurn) {
+					System.out.print("Choose your space: ");
+					input[0] = userInput.nextInt();
+					input[1] = userInput.nextInt();
+					game.playerTurn(input);
+					System.out.println(game.getBoard());
+					playerTurn = false;
+				}else if (!playerTurn) {
+					game.botTurn();
+					System.out.println(game.getBoard());
+					playerTurn = true;
+				}
+				win = game.checkWin();
+				draw = game.checkDraw();
+			}
+			if (!playerTurn && win) {
+				System.out.println("Congratulations! You win!");
+			}else if (playerTurn && win) {
+				System.out.println("Darn you lost.");
+			}else {
+				System.out.println("Draw.");
+			}
+			System.out.println("Would you like to play again?\nIf so, input a number greater than 0.");
+			x = userInput.nextInt();
+			if (x > 0) {
+				game.reset();
+			}else {
+				playing = false;
+			}
+		}
+		
+		System.out.println("Thanks for playing!");
+		
+		userInput.close();
+		
 	}
 
 }
